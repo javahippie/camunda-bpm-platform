7d2a2f3fba40fee4b93fbd3234b3138958a0f4bb
Daniel Kelemen <daniel.kelemen@camunda.com>
2022-03-18 15:07:47 +0100

    feat(engine/admin): rename metrics to match sales wording
    - remove flow-node-instances-end
    - rename flow-node-instances-start
    - adapt code & tests
    
    related to CAM-13636

--- engine/src/main/java/org/camunda/bpm/engine/impl/metrics/util/MetricsUtil.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/metrics/util/MetricsUtil.java
@@ -31,8 +31,7 @@ public class MetricsUtil {
       case Metrics.TASK_USERS: return Metrics.UNIQUE_TASK_WORKERS;
       case Metrics.PROCESS_INSTANCES: return Metrics.ROOT_PROCESS_INSTANCE_START;
       case Metrics.DECISION_INSTANCES: return Metrics.EXECUTED_DECISION_INSTANCES;
-      case Metrics.FLOW_NODE_INSTANCES_START: return Metrics.ACTIVTY_INSTANCE_START;
-      case Metrics.FLOW_NODE_INSTANCES_END: return Metrics.ACTIVTY_INSTANCE_END;
+      case Metrics.FLOW_NODE_INSTANCES: return Metrics.ACTIVTY_INSTANCE_START;
       default: return publicName;
     }
   }
@@ -48,8 +47,7 @@ public class MetricsUtil {
       case Metrics.UNIQUE_TASK_WORKERS: return Metrics.TASK_USERS;
       case Metrics.ROOT_PROCESS_INSTANCE_START: return Metrics.PROCESS_INSTANCES;
       case Metrics.EXECUTED_DECISION_INSTANCES: return Metrics.DECISION_INSTANCES;
-      case Metrics.ACTIVTY_INSTANCE_START: return Metrics.FLOW_NODE_INSTANCES_START;
-      case Metrics.ACTIVTY_INSTANCE_END: return Metrics.FLOW_NODE_INSTANCES_END;
+      case Metrics.ACTIVTY_INSTANCE_START: return Metrics.FLOW_NODE_INSTANCES;
       default: return internalName;
     }
   }
